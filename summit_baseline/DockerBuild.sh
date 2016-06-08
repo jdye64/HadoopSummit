64982b31e282731c9bae92cd8db3c97ae2455e94
Jeremy Dyer <jdye64@gmail.com>
2016-06-07 20:22:43 -0400

    Added .gitignore file to ensure that sensitive files are not placed in public facing Github

--- summit_baseline/DockerBuild.sh
+++ summit_baseline/DockerBuild.sh
@@ -1,3 +1,3 @@
 echo "Creating Hadoop Summit Baseline Image"
 eval "$(docker-machine env docker-hwx)"
-docker build -t jdye64/summit_baseline .
+docker build -t jdye64/hadoopsummit:summit_baseline .
---
+++ summit_baseline/README.md
@@ -0,0 +1,4 @@
+Holds the baseline Docker images that are built upon by the other images and hold common components.
+
+BASELINE->summit_rpi
+BASELINE->summit_osx
