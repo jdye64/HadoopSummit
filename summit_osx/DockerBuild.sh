b2d178b90a76defbda2b1c22fa48c695a5b3f679
Jeremy Dyer <jdye64@gmail.com>
2016-06-14 12:33:46 -0400

    introducing summit_osx docker image

--- summit_osx/DockerBuild.sh
+++ summit_osx/DockerBuild.sh
@@ -1,3 +1,3 @@
 echo "Creating Hadoop Summit OS X Image"
 eval "$(docker-machine env docker-hwx)"
-docker build -t jdye64/hadoopsummit:summit_osx .
+docker build --no-cache -t jdye64/hadoopsummit:summit_osx .
